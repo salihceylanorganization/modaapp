9fa2bc27cd605d9bb49c109172210332d7c46be2
salihceylan <salihceylan@gmail.com>
2020-08-09 23:25:16 +0300

    Moda Uygulaması

--- lib/detay.dart
+++ lib/detay.dart
@@ -13,15 +13,35 @@ class _DetayState extends State<Detay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: widget.imgPath,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
-        ),
+      body: Stack(
+        children: [
+          Hero(
+            tag: widget.imgPath,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            bottom: 15,
+            left: 15,
+            right: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(15),
+              color:Colors.white.withOpacity(0.7) ,
+              elevation: 4,
+              child: Container(
+                width: 300,
+                height: 200,
+                child: Text("Tmm"),
+
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
